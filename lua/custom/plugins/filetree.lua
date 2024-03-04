f56c104bfdbb0ed16cb0f082afdbc23804102ca2
Corné <code@jammo.nl>
2024-03-04 21:52:33 +0100

    Update config

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,46 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+
+    vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', { silent = true })
+
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      enable_git_status = true,
+      enable_diagnostics = true,
+      modified = {
+        symbol = '[+]',
+        highlight = 'NeoTreeModified',
+      },
+      git_status = {
+        symbols = {
+          -- Change type
+          added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+          modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+          deleted = '✖', -- this can only be used in the git_status source
+          renamed = '󰁕', -- this can only be used in the git_status source
+          -- Status type
+          untracked = '',
+          ignored = '',
+          unstaged = '󰄱',
+          staged = '',
+          conflict = '',
+        },
+      },
+    }
+  end,
+}
