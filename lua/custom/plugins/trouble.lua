451df0bc3ddb4e47deada094c2e7fa3fabbf8bf9
Corné <46375984+jammo-nl@users.noreply.github.com>
2024-03-05 14:22:50 +0100

    Add trouble plugin / update keymaps

---
+++ lua/custom/plugins/trouble.lua
@@ -0,0 +1,20 @@
+return {
+  'folke/trouble.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>tx', function()
+      require('trouble').toggle()
+    end, { desc = 'Toggle trouble' })
+    vim.keymap.set('n', '<leader>tw', function()
+      require('trouble').toggle 'workspace_diagnostics'
+    end, { desc = 'Toggle workspace diagnostics' })
+    vim.keymap.set('n', '<leader>tt', function()
+      require('trouble').toggle 'todo'
+    end, { desc = 'Toggle todo' })
+  end,
+}
