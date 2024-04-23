74f09d3b3868d683e94a628124946ea4c08f38c6
Corné <code@jammo.nl>
2024-04-23 17:01:24 +0200

    Update list of installed / configured lsp's

--- init.lua
+++ init.lua
@@ -571,7 +571,10 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
+	goimports = {},
+	html = {},
+	cssls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -580,7 +583,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
         --
 
         rust_analyzer = {
