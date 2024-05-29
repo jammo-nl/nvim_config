1e60487c98430beed474e232a24ca24e9cb6009e
Corné <46375984+jammo-nl@users.noreply.github.com>
2024-05-29 12:56:57 +0200

    Add keymap description

--- lua/custom/plugins/crates.lua
+++ lua/custom/plugins/crates.lua
@@ -5,8 +5,8 @@ return {
     require('crates').setup()
     local crates = require("crates")
 
-    vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, { silent = true })
-    vim.keymap.set("n", "<leader>cf", crates.show_features_popup, { silent = true })
-    vim.keymap.set("n", "<leader>cu", crates.upgrade_crate, { silent = true })
+    vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, { silent = true, desc = "Show crate versions" })
+    vim.keymap.set("n", "<leader>cf", crates.show_features_popup, { silent = true, desc = "Show crate features" })
+    vim.keymap.set("n", "<leader>cu", crates.upgrade_crate, { silent = true, desc = "Upgrade crate" })
   end,
 }
