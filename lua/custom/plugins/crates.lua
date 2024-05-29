b5a337deac8a9645b3c6d5323f97ea26f79f8d95
Corné <46375984+jammo-nl@users.noreply.github.com>
2024-05-29 12:51:10 +0200

    Add crate update keymap

--- lua/custom/plugins/crates.lua
+++ lua/custom/plugins/crates.lua
@@ -3,5 +3,8 @@ return {
   tag = 'stable',
   config = function()
     require('crates').setup()
+    local crates = require("crates")
+
+    vim.keymap.set("n", "<leader>cu", crates.update_crate, { silent = true })
   end,
 }
